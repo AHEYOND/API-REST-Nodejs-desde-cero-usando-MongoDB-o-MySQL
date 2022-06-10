2c6eacbdc5e4cbc4a6b54473c14f9740700a68ec
AHEYOND <aheyond@gmail.com>
2022-06-10 18:22:33 -0500

    Modelos (MongoDB)

---
+++ config/mongo.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const dbConect = () => {
+  const DB_URI = process.env.DB_URI;
+  mongoose.connect(
+    DB_URI,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err, res) => {
+      if (!err) {
+        console.log("Conexión Corecta")
+      } else {
+        console.log("Error de conexión")
+      }
+    }
+  );
+};
+
+module.exports = dbConect;
