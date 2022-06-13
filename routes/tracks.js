704abb83838e38239a0f7241e0915fe6c6552684
AHEYOND <aheyond@gmail.com>
2022-06-12 21:34:58 -0500

    Middleware

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const {validatorCreateItem}=require("../validators/tracks")
-const {getItems, getItem, createItem}=require("../controllers/tracks")
+const customHeader = require("../middleware/customHeader");
+const {validatorCreateItem}=require("../validators/tracks");
+const {getItems, getItem, createItem}=require("../controllers/tracks");
 //http://localhost/tracks GET, POST, DELETE, PUT
 
 router.get("/", getItems);
-router.post("/", validatorCreateItem, createItem);
+router.post("/", validatorCreateItem, customHeader, createItem);
 
 module.exports = router;
