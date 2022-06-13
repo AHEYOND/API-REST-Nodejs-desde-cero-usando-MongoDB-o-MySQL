1c8e7625ddc4def3d73afada15364d30cfe16ba7
AHEYOND <aheyond@gmail.com>
2022-06-12 21:14:12 -0500

    Validación de datos

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const {validatorCreateItem}=require("../validators/tracks")
 const {getItems, getItem, createItem}=require("../controllers/tracks")
 //http://localhost/tracks GET, POST, DELETE, PUT
 
-router.get("/", getItems)
-router.post("/", createItem)
+router.get("/", getItems);
+router.post("/", validatorCreateItem, createItem);
 
-module.exports = router
+module.exports = router;
